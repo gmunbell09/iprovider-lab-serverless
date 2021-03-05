87cb53e1a6941ccd12182ec20f0f7275f0c84c1d
giusseppe.munoz <giusseppe.dev@gmail.com>
2021-03-04 20:21:56 -0500

    Update function.mk

--- makefiles/function.mk
+++ makefiles/function.mk
@@ -4,7 +4,12 @@ APP_DIR           = app
 IMAGE_BUILD       = mcr.microsoft.com/dotnet/core/sdk
 
 build:
-	dotnet publish /${APP_DIR}/src/serverless-apirest/serverless-apirest.csproj -o /${APP_DIR}/release
+	docker container run --env DOTNET_CLI_HOME=/tmp/ \
+		--workdir "/${APP_DIR}" --rm -i \
+		-u ${UID_LOCAL}:${GID_LOCAL} \
+		-v "${PWD}/${APP_DIR}":/${APP_DIR} \
+		${IMAGE_BUILD} \
+		dotnet publish /${APP_DIR}/src/serverless-apirest/serverless-apirest.csproj -o /${APP_DIR}/release
 
 create.zip:
 	@cd ${PWD}/${APP_DIR}/release && zip -r9 $(PROJECT_NAME).zip *
